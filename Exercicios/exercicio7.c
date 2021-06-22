dc6aa442f3d3037488bc6e3ff3825005d2f0d5c4
Thallysson Ravel <74942957+RavelS18@users.noreply.github.com>
2021-06-22 16:33:31 -0300

    Calcular Distancia

--- Exercicios/exercicio7.c
+++ Exercicios/exercicio7.c
@@ -17,7 +17,7 @@ void calcular_distancia(){
         printf("Agora digite a coordenada y do %i ponto\n", i+1);
         scanf("%f", &y[i]);
     }
-    //Expressão que calcula a distancia
+    //Expressão que calcula a distância
     calculo_da_distancia = sqrt((x[1]-x[0])*(x[1]-x[0])+(y[1]-y[0])*(y[1]-y[0]));
     printf("A distancia entre os pontos apresentados e: %f\n", calculo_da_distancia);
 }
