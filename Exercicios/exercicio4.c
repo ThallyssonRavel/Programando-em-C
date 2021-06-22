410864dfc985d53aa31baa0ff93995270463e1c7
Thallysson Ravel <74942957+RavelS18@users.noreply.github.com>
2021-06-22 14:48:23 -0300

    Operações matemáticas: potenciação e radiciação.

--- Exercicios/exercicio4.c
+++ Exercicios/exercicio4.c
@@ -1,4 +1,3 @@
-//Bibliotecas utilizadas
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -10,36 +9,28 @@
 //Caso o numero for negativo, o programa ira apreserntar o resultado 0a cada uma das operaçoes;
 
 int main(void){
-    //Variaveis utilizadas
     int numero, quadrado, cubo, raiz;
-    //Mensagens para incio do programa
     printf("Seja muito bem vindo(a)\n");
     printf("Para comercamos");
 
     printf("digite um numero");
     scanf("%i", &numero);
-    //Inicio das validações
+    
     if(numero>0){
-        //Caso o numero for positivo, ira realizar tais operaçoes abaixo
         quadrado = numero*numero;
         cubo = numero*numero*numero;
         raiz = sqrt(numero);
-        //Aqui apresenta o resultado de cada uma
         printf("O numero %i ao quadrado e: %i\n", numero, quadrado);
         printf("O numero %i ao cubo e: %i\n", numero, cubo);
         printf("A raiz do numero %i e: %i\n", numero, raiz);
 
     }
     if(numero<0){
-        //Caso o numero seja negativo, ira realizar tais operaçoes abaixo
         quadrado = 0;
         cubo = 0;
         raiz = 0;
-        //Aqui apresenta o resultado de cada uma
         printf("O numero %i ao quadrado e: %i\n", numero, quadrado);
         printf("O numero %i ao cubo e: %i\n", numero, cubo);
         printf("A raiz do numero %i e: %i\n", numero, raiz);
     }
-  
-
 }
